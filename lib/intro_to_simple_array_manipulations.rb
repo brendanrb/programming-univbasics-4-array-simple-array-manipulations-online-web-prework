d11ab1a5d4378ac19e3160f735338f2f4605ab1b
Brendan R Borgers <brendanborgers@gmail.com>
2019-11-21 19:52:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(my_favorite_things, more_favs)
   all_my_faves = my_favorite_things.concat(more_favs)
 end
 
-def using_insert(list_of_programming_languages, another_language)
+def using_insert(list_of_programming_languages)
   new_array = list_of_programming_languages.insert(another_language)
 end
   
