ad7a4124f6eff030d2920d9817ccfe4855b29786
Brendan R Borgers <brendanborgers@gmail.com>
2019-11-21 19:39:24 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,37 @@
+def using_push(array, string)
+ array.push("colors_in_the_rainbow", "violet")
+end
+
+def using_unshift(array, string)
+  array.unshift("Staten Island")
+end
+
+def using_pop(continents)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents.pop
+  
+end
+
+def pop_with_args(dog_breeds)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  dog_breeds.pop(2)
+end
+
+def using_shift(my_favorite_cities)
+  
+  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  my_favorite_cities.shift 
+  
+end
+
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+ice_cream_brands.shift(2)
+end
+
+def using_concat(my_favorite_things, more_favs) 
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["sports cars", "flatiron school"]
+    my_favorite_things.concat[more_favs]
+end
+
